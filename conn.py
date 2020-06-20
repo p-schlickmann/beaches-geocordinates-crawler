1c2aaa8b92df69604690c3c9c5938fac3fb2e38f
Pedro Schlickmann Mendes <windowsxpedro@gmail.com>
2020-06-20 13:30:04 -0300

    Create conn.py

---
+++ conn.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+
+class DatabaseConnection:
+    def __init__(self, host):
+        self.connection = None
+        self.host = host
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.host)
+        cursor = self.connection.cursor()
+        return cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.commit()
+        self.connection.close()
